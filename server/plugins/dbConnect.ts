e2fc76022cf59a36fd92dd6b54e3b3aba30dcfeb
Kylin <107255387+kylin22@users.noreply.github.com>
2024-11-19 20:21:27 +1100

    fix: useRuntimeConfig() outside lifecycle hook

--- server/plugins/dbConnect.ts
+++ server/plugins/dbConnect.ts
@@ -1,8 +1,9 @@
 import mongoose from "mongoose";
 import { color } from "../../src/utils/logColors";
+import { useNuxtApp } from "nuxt/app";
 
 export default async () => {
-  const config = useRuntimeConfig();
+  const config = useNuxtApp().$config
   const MONGO_URI = config.dbURI as string;
     if (!MONGO_URI) return console.log(color("text",`🍃 Mongo URI not found, ${color("error", "skipping.")}`))
     mongoose.connect(`${MONGO_URI}`)
